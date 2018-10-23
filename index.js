832daf0aef6a6cb4cadb01c60c5c5d70b26d35ad
aditya20apr <aditya20apr@gmail.com>
2018-10-23 23:10:42 +0530

    ss

--- index.js
+++ index.js
@@ -5,15 +5,26 @@
 
 // Dependencies
 var server = require('./lib/server');
+//var workers = require('./lib/workers');
+var cli = require('./lib/cli');
 
 // Declare the app
 var app = {};
 
 // Init function
-app.init = function () {
+app.init = function(){
 
   // Start the server
   server.init();
+
+  // Start the workers
+  //workers.init();
+
+  // Start the CLI, but make sure it starts last
+  setTimeout(function(){
+    cli.init();
+  },50);
+
 
 };
 
